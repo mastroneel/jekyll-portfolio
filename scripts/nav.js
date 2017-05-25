7e258e567a25a7728f8d1e222e0a7ef41c52451e
Lauren Mastroni <laurenmastroni@gmail.com>
2017-05-25 18:30:57 +0800

    Add nav animations

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,3 +1,7 @@
+// *********************
+// Nav dropdown + toggle
+// *********************
+
 (function($) {
   $(function() {
     $('nav ul li a:not(:only-child)').click(function(e) {
@@ -16,3 +20,80 @@
     });
   });
 })(jQuery);
+
+
+// ********************
+// Shrink nav on scroll
+// ********************
+
+$(function(){
+    $('.navigation').data('size','big');
+});
+
+$(window).scroll(function(){
+    if($(document).scrollTop() > 0)
+    {
+        if($('.navigation').data('size') == 'big')
+        {
+            $('.navigation').data('size','small');
+            $('.navigation').stop().animate({
+                height: '55px',
+                lineHeight: '35px'
+            },100);
+            $('.nav-list').css({
+              lineHeight: '55px'
+            });
+            $('.logo a').css({
+              color: '#262626',
+              fontSize: '30px',
+            });
+            $('.navigation').css('background-color', 'rgba(255, 255, 255, 1)');
+            $('nav ul li a').css('color', '#262626');
+        }
+    }
+    else
+    {
+        if($('.navigation').data('size') == 'small')
+        {
+            $('.navigation').data('size','big');
+            $('.navigation').stop().animate({
+                height: '85px',
+                lineHeight: '85px'
+            },100);
+            $('.nav-list').css({
+              lineHeight: '85px'
+            }, 100);
+            $('.logo a').css({
+              color: '#fff',
+              fontSize: '50px'
+            }, 100);
+            $('.navigation').css({backgroundColor: 'rgba(0, 0, 0, 0.2)'}, 100);
+            $('nav ul li a').css({color: '#fff'}, 100);
+        }
+    }
+});
+
+
+// $(document).ready(function(){
+//             var scroll_pos = 0;
+//             $(document).scroll(function() {
+//                 scroll_pos = $(this).scrollTop();
+//                 if(scroll_pos > 2) {
+//                     $('.navigation').css('background-color', 'rgba(255, 255, 255, 1)');
+//                     $('nav ul li a').css('color', '#262626');
+//                     $('nav ul li a:visited').css('color', '#262626');
+//                     // $('.logo a').css({
+//                     //   color: '#262626',
+//                     //   fontSize: '30px',
+//                     // });
+//                 } else {
+//                     $('.navigation').css('background-color', 'rgba(0, 0, 0, 0.2)');
+//                     $('nav ul li a').css('color', '#fff');
+//                     $('nav ul li a:visited').css('color', '#fff');
+//                     // $('.logo a').css({
+//                     //   color: '#fff',
+//                     //   fontSize: '50px'
+//                     // });
+//                 }
+//             });
+//         });
